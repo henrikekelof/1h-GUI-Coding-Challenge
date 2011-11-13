c105f9d9ecf6e2594920d96958689abea90ecd5c
Henrik Ekelöf <henrik@hmail.se>
2011-11-13 22:54:29 +0100

    Added zoom-fix for iOS

--- js/main.js
+++ js/main.js
@@ -2,6 +2,24 @@
 
 var GC = GC || {};
 
+/* 
+Fix för iPhone:s zoom-bugg vid orientation change
+http://adactio.com/journal/4470/
+*/
+(function () {
+    "use strict";
+    if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i)) {
+        var viewportmeta = document.querySelector('meta[name="viewport"]');
+        if (viewportmeta) {
+            viewportmeta.content = 'width=device-width,minimum-scale=1.0, maximum-scale=1.0';
+            document.body.addEventListener('gesturestart', function () {
+                viewportmeta.content = 'width=device-width, minimum-scale=0.25, maximum-scale=1.6';
+            }, false);
+        }
+    }
+}());
+
+
 GC.namespace('GC.util');
 GC.util = (function () {
     "use strict";
